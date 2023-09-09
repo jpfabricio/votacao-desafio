5afa5969fc3bd75be632ded20aa9aec890f5cff0
jpfabricioSMU <88002729+jpfabricioSMU@users.noreply.github.com>
2023-09-09 14:33:24 -0300

    First Commit

---
+++ src/main/java/br/com/brq/votacao/VotacaoApplication.java
@@ -0,0 +1,20 @@
+package br.com.brq.votacao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Slf4j
+@SpringBootApplication
+public class VotacaoApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(VotacaoApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoApplication.class, args);
+	}
+}
